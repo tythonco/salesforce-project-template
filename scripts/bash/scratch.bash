06f4f2c7ed3223fc177570bfa7ac611fbfc9ee7f
Charles Ross <chivalry@mac.com>
2024-03-14 14:30:39 -0700

    Update scratch script to use sf

--- scripts/bash/scratch.bash
+++ scripts/bash/scratch.bash
@@ -1,10 +1,17 @@
 #1/bin/bash
 
+# Executes the start script to create a scratch org and push the source code, but then (while
+# perhaps returning the user to the calling terminal) takes note of the current git branch,
+# creates an alias to the default org that is the same as that branch, allowing the use of
+# `npm run checkout` to move simultaneously between branches and matching orgs.`
+
 source scripts/bash/utils.bash
 
 npm run start
 focus_caller
 branch=$(git branch --show-current)
-org=$(sfdx force:org:display --json | grep -o '"username": "[^"]*' | grep -o '[^"]*$')
-sfdx alias:set ${branch}=${org}
+org=$(sf org display --json \
+    | grep -o '"username": "[^"]*' \
+    | grep -o '[^"]*$')
+sf alias set ${branch}=${org}
 npm run checkout ${branch}
