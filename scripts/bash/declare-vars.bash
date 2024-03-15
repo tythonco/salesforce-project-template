164a0339d81ac4604344d661a7a3469610aeb6fb
Charles Ross <chivalry@mac.com>
2024-03-15 14:33:41 -0700

    Clean up var declare

--- scripts/bash/declare-vars.bash
+++ scripts/bash/declare-vars.bash
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+source scripts/bash/utils.bash
+
 apex_setup="$(apex_setup)"
 devhub_name="$(devhub_name)"
 err_email="$(err_email)"
 perm_sets="$(perm_sets)"
 pkg_name="$project_name"
-pkg_path=$(node --eval "console.log(require('./sfdx-project.json').packageDirectories[0].path);")
-pkg_type=$(pkt_type)
+pkg_path=$(node --eval "
+    console.log(
+        require(
+            './sfdx-project.json'
+    ).packageDirectories[0].path);"
+)
+pkg_type=$(pkg_type)
 project_name="$(project_name)"
 sfdx_auth_file="$(auth_dir)/sfdx$(auth_file_suffix)"
